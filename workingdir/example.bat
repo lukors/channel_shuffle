0e9ef2a9d6b0e93f540b8765cebc8de76af1b69a
Lukas Orsvärn <lukas@orsvarn.com>
2015-01-31 16:57:30 +0100

    Creates a workingdir directory & adds missing files
    Moves the readme file from document to workingdir. Adds example.bat and
    default images to workingdir.
    
    It should now be easier to put together a release since you can just add
    all the required .dll files from Qt and the .exe files to workingdir and
    then it should be ready for release.

---
+++ workingdir/example.bat
@@ -0,0 +1,10 @@
+@rem Example 1
+channel_shuffle image_2.tga output_1.bmp -g 1r -r 1a
+
+@rem Example 2
+channel_shuffle image_2.tga image_1.tga output_2.tga -width 100 -height 50 -r 1r -g 1g -b 1b -a 2a
+
+@rem Example 3
+channel_shuffle image_2.tga output_3.dds -r 1ri -g 1ri -b 1ri
+
+@pause
